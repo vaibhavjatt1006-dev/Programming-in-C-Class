e0f6796f98a5e5e644ee149855eb6e89fe3dff7a
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-09-13 11:28:31 +0530

    Add files via upload

---
+++ Simple Interest .c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+// finding_the simple interest
+
+int main()
+
+float P,R,T;
+
+printf("Enter Principle amount :");
+
+scanf("%f",&P);
+
+printf("Enter Rate :");
+
+scanf("%f",&R);
+
+printf("Enter Time Period: ");
+
+scanf("%f",&T);
+
+float SI;
+
+printf("Simple Interest : %f\n", SI=P*R*T/100);
+
+printf("My name is Vaibhav");
+
+return 0;
+
+}
