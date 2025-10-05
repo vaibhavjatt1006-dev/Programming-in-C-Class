5c4df6062167e64e6073d4630fcefe1cb0540bf0
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-05 16:34:03 +0530

    Add files via upload

---
+++ Sum no. until user enter 0.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main () {
+int i, n,sum;
+
+	printf("Enter no. n:");
+	scanf("%d",&n);
+	sum=n;
+for(i=0;n!=0;i++) {
+	
+	printf("Enter no. n:");
+	scanf("%d",&n);
+	sum=sum+n;
+}
+
+printf("sum of number is %d\n",sum);
+printf("Vaibhav 125113039");
+
+return 0;
+}
