f33f407117563141a1cbaaaf14cf554547322c51
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-05 09:38:08 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float r,a;
+printf("Enter radius:");
+scanf("%f", &r);
+a=3.14*r*r;
+printf("Area of circle is %f\n", a);
+printf("Vaibhav 125113039");
+return 0;
+}
