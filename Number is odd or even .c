f6c343ed0dd5bb6922b9699e760c961e7338d108
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-10-07 19:22:15 +0530

    Add files via upload

---
+++ Number is odd or even .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+int a;
+printf("ENTER NUMBER TO CHECK EVEN OR ODD:");
+scanf("%d", &a);
+
+if(a%2==0)
+{
+printf("NUMBER IS EVEN");
+}
+
+ else
+ {
+printf("NUMBER IS ODD");
+}
+
+printf("Vaibhav 125113039\n");
+return 0;
+}
