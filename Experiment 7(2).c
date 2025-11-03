d640af3472ff7284467a701ee32cd2c93869f6fa
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-11-03 18:22:37 +0530

    Add files via upload

---
+++ Experiment 7(2).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 20);
+    printf("Vaibhav 125113039");
+    return 0;
+}
