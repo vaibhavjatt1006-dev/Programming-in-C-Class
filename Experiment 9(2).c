bbb76840827c736fec1e0eb9c7fefa6c3538a33c
vaibhavjatt1006-dev <vaibhavjatt1006@gmail.com>
2025-11-03 18:33:08 +0530

    Add files via upload

---
+++ Experiment 9(2).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[4] = {10, 20, 30, 40};
+
+    for (int i = 0; i < 4; i++)
+    {
+        a[i]++;
+    }
+
+    printf("INCREMENTED ARRAY IS :");
+
+    for (int j = 0; j < 4; j++)
+    {
+        printf("%d ", a[j]);
+    }
+
+    printf("\n");
+
+    printf("Vaibhav 125113039");
+
+    return 0;
+}
